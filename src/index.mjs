46b9ac2957062cf33dc02ffbcb7f7dca92ffa763
fujino <83009750+akira-fujino18@users.noreply.github.com>
2023-09-18 10:00:09 +0000

    Update 3 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,68 @@
+import "./styles.css";
 
-/**const.letなどの変数宣言 */
+const onClickadd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-var vai1 = "var変数";
-console.log(val1);
+  // 未完了リストにTODOを追加
+  createIncompleteList(inputText);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = text;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // TODO内容テキストを取得
+    const text = li.innerText;
+    // div以下を初期化
+    div.textContent = null;
+    // liタグ生成
+    const newLi = document.createElement("li");
+    newLi.innerText = text;
+
+    // buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ(div)を完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキスト取得
+      const text = deleteTarget.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+
+    div.appendChild(newLi);
+    div.appendChild(backButton);
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(div);
+  });
+
+  // 削除ボタンを追加
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("imcomplete-list").removeChild(deleteTarget);
+  });
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  // 未完了リストに追加
+  document.getElementById("imcomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickadd());
